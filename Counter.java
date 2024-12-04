4bfadb1b5dabf3f5658dc60ab460cbb95de07fb0
BugHunter-X <askzenai@gmail.com>
2024-12-04 05:46:07 +0100

    Add Counter.java file

---
+++ Counter.java
@@ -0,0 +1,34 @@
+public class Counter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+        System.out.println(counter.getCount()); // Expected: 20000, but might be less due to race condition
+    }
+}
